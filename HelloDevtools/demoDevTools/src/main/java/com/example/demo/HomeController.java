a27d8e94138b0be7afa0cc9398a73d2789715049
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-05-14 12:03:01 -0500

    Using properties.xml file

--- HelloDevtools/demoDevTools/src/main/java/com/example/demo/HomeController.java
+++ HelloDevtools/demoDevTools/src/main/java/com/example/demo/HomeController.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 	
+	@Value("${pageController.msg}")
+	private String pageController;
+	
 	@RequestMapping("/")
 	public String home() {
-		return "Hey KNARF, here we are creating some Spring Boot dev-tools!";
+		return this.pageController;
 	}
 
 }
